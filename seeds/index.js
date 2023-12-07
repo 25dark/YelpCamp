4380b65cbebcaa9686a1ebe0b835b46b12085f02
25Dark <henrydanso6@gmail.com>
2023-12-07 15:59:45 +0000

    seeding campground

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const Campground = require("../models/campground");
+
+main().catch((err) => {
+  console.log("ON NO MONGO CONNECTION ERROR");
+  console.log(err);
+});
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("Database connected");
+}
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB();
